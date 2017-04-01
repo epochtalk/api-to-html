6f7c33aefd5dda0ad5b000a67c259b3f778bb847
Anthony Kinsey <anthony@slickage.com>
2017-03-31 15:34:46 -1000

    Added Promises. Made generate route functional
    * Generate route now generates docs, then generates the html
      output

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 'use strict';
 require('dotenv').load();
 var path = require('path');
+var Joi = require('joi');
+var Boom = require('boom');
+var Promise = require('bluebird');
+var generateHtml = require(path.join(__dirname, 'generate-html'));
+var createDoc = Promise.method(require('apidoc').createDoc);
+
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
 var epochtalkPath = process.env.EPOCHTALK_PATH;
 var apiKey = process.env.API_KEY;
+
 if (!epochtalkPath) {
   console.error('No EPOCHTALK_PATH in ENV!');
   process.exit(1);
@@ -15,41 +25,36 @@ if (!apiKey) {
   process.exit(1);
 }
 
-const Hapi = require('hapi');
-const Joi = require('joi');
-const Boom = require('boom');
-const apiDoc = require('apidoc');
-
-const server = new Hapi.Server();
-
 server.connection({ port: 3000, host: 'localhost' });
 
 server.route({
-    method: 'POST',
-    path: '/generate',
-    config: {
-      validate: {
-        payload: Joi.object().keys({
-          api_key: Joi.string().required()
+  method: 'POST',
+  path: '/generate',
+  config: {
+    validate: {
+      payload: Joi.object().keys({
+        api_key: Joi.string().required()
+      })
+    },
+    handler: function (request, reply) {
+      if (request.payload.api_key === apiKey) {
+        var opts = { src: epochtalkPath,  dest: 'doc', excludeFilters: ['node_modules', 'public'] };
+        return createDoc(opts)
+        .then(function() {
+          return generateHtml.generate()
+          .then(function(results) { reply(results); })
+          .catch(function(e){ reply(Boom.badImplementation(e.message)); });
         })
-      },
-      handler: function (request, reply) {
-        if (request.payload.api_key === apiKey) {
-          var opts = { src: epochtalkPath,  dest: 'doc', excludeFilters: ['node_modules', 'public'] };
-          if (apiDoc.createDoc(opts)) {
-            reply('Docs Generated');
-          }
-          else { reply(Boom.badImplementation('Error: api doc generation failed')); }
-        }
-        else { reply(Boom.unauthorized('Invalid key.  Go fuck yourself')); }
+        .catch(function() {
+          reply(Boom.badImplementation('Error: api doc generation failed'));
+        });
       }
+      else { reply(Boom.unauthorized('Invalid API Key. Go fuck yourself')); }
     }
+  }
 });
 
 server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
+  if (err) { throw err; }
+  console.log(`Server running at: ${server.info.uri}`);
 });
