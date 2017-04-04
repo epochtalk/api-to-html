4f46f41b72200ec5d1efaac75fbbdf36a06b911c
Anthony Kinsey <anthony@slickage.com>
2017-04-03 15:00:31 -1000

    Add index entry point to generate docs

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+require('dotenv').load();
+var path = require('path');
+var Promise = require('bluebird');
+var generateHtml = require(path.join(__dirname, 'generate-html'));
+var createDoc = Promise.method(require('apidoc').createDoc);
+var epochtalkPath = process.env.EPOCHTALK_PATH;
+
+var opts = { src: epochtalkPath,  dest: 'doc', excludeFilters: ['node_modules', 'public'] };
+
+return createDoc(opts)
+.then(function() {
+  return generateHtml.generate()
+  .then(function(results) { console.log(results); })
+  .catch(function(e){ console.log('Error:', e.message); });
+})
+.catch(function() {
+  console.log('Error: api doc generation failed');
+});
